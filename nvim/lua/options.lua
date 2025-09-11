416ee36d4a387c4fd87498d30948d1f39145d0e8
blue-emc2 <s.yamazaki@everyleaf.com>
2025-09-11 10:10:02 +0900

    カーソルを無効

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,3 +3,8 @@ local opt = vim.opt
 -- 行番号の表示
 opt.number = true
 opt.relativenumber = true
+
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")

cd80b10c81b4f552897614f1590ae9c3f6e1a5e3
blue-emc2 <s.yamazaki@everyleaf.com>
2025-09-17 18:19:00 +0900

    ファイルタイプを判別する設定を追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,7 @@ require("keymaps")
 vim.o.clipboard = "unnamedplus"
 vim.o.termguicolors = true
 vim.o.fileencodings = "utf-8,euc-jp"
+vim.o.fileencoding = "utf-8"
+
+vim.opt.list = true
+vim.opt.listchars = "tab:>-"

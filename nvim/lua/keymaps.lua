cc4075c294521a08d10796b03cfbbe6e96782cf5
blue-emc2 <s.yamazaki@everyleaf.com>
2025-09-19 22:42:43 +0900

    ターミナルモードからノーマルモードに移る時のキーマップを変更

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,6 +8,15 @@ vim.keymap.set("n", "<Up>", "<Nop>")
 vim.keymap.set("n", "<Down>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 vim.keymap.set("n", "<Right>", "<Nop>")
+
+vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
+
+-- LSP関連のキーマッピング
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "Find references" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
+
 -- barbar.nvim のキーマッピング
 vim.keymap.set("n", "<C-x>", "<Cmd>BufferClose<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<C-p>", "<Cmd>BufferPrevious<CR>", { desc = "Previous buffer" })

ce6afa018eca7b1ccf079b54e0586fc5b333c689
blue-emc2 <s.yamazaki@everyleaf.com>
2025-09-20 09:56:17 +0900

    インデント自動調整

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,15 @@ vim.o.fileencoding = "utf-8"
 
 vim.opt.list = true
 vim.opt.listchars = "tab:>-"
+
+-- インデントを自動で調整
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- タブをスペースに変換する
+vim.opt.expandtab = true
+
+-- インデントの幅を設定
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
